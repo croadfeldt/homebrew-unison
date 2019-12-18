832dc36bda414eefba5e69e4d9577540dc0071e3
croadfeldt <chris@roadfeldt.com>
2019-12-18 01:35:17 -0600

    Use correct URL for tar.gz file.

--- unison.rb
+++ unison.rb
@@ -1,8 +1,8 @@
 class Unison < Formula
   desc "File synchronization tool for OSX"
   homepage "https://www.cis.upenn.edu/~bcpierce/unison/"
-  url "https://github.com/bcpierce00/unison/commit/26a29f7.diff?full_index=1"
-  sha256 "a7f2da810599c1990ba8c7fb70c2b7723aae8543170707c45f4ac7a6ee0f4645"
+  url "https://github.com/bcpierce00/unison/archive/26a29f79487484b7982c85e0dc879cf7aaaf584f.tar.gz"
+  sha256 "4b5dd5af7354cce25b261d0e7106df76b2d0386898f78e0ab952580907e9c757"
   head "https://github.com/bcpierce00/unison.git", :branch => "master"
 
   depends_on "ocaml" => :build
