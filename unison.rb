86773a6b7acb1c8e3d03bb26e0574c87042e6d3d
croadfeldt <chris@roadfeldt.com>
2019-12-18 01:46:50 -0600

    Add version tag.

--- unison.rb
+++ unison.rb
@@ -1,5 +1,6 @@
 class Unison < Formula
   desc "File synchronization tool for OSX"
+  version "2.51.3-git.26a29f7"
   homepage "https://www.cis.upenn.edu/~bcpierce/unison/"
   url "https://github.com/bcpierce00/unison/archive/26a29f79487484b7982c85e0dc879cf7aaaf584f.tar.gz"
   sha256 "4b5dd5af7354cce25b261d0e7106df76b2d0386898f78e0ab952580907e9c757"
