19a31fa307368ed08b915b8e4fc07501b080445d
croadfeldt <chris@roadfeldt.com>
2019-12-18 01:31:05 -0600

    Make 2.51.3 the default.

--- unison.rb
+++ unison.rb
@@ -1,17 +1,9 @@
 class Unison < Formula
   desc "File synchronization tool for OSX"
   homepage "https://www.cis.upenn.edu/~bcpierce/unison/"
-  url "https://github.com/bcpierce00/unison/archive/v2.51.2.tar.gz"
-  sha256 "a2efcbeab651be6df69cc9b253011a07955ecb91fb407a219719451197849d5e"
+  url "https://github.com/bcpierce00/unison/commit/26a29f7.diff?full_index=1"
+  sha256 "a7f2da810599c1990ba8c7fb70c2b7723aae8543170707c45f4ac7a6ee0f4645"
   head "https://github.com/bcpierce00/unison.git", :branch => "master"
-
-  bottle do
-    cellar :any_skip_relocation
-    rebuild 1
-    sha256 "045499eab89d6b0d3faa898fe91c71bc7cba031c5a244c2f7367bc3825c958b5" => :catalina
-    sha256 "58e3ccf3e3f0ac6b331b786fdb7bbe9f0e22babec094279f2c715bef256daacb" => :mojave
-    sha256 "0cce7f269f0458a4ce1318fce5af58f9bcbfd29e314211fc8dedb6b4d8a7f1fb" => :high_sierra
-  end
 
   depends_on "ocaml" => :build
 
