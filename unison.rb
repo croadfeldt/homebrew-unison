4a5c5b088b9342d6c80159f77b244439c5e9d1dd
croadfeldt <chris@roadfeldt.com>
2020-11-15 01:02:30 -0600

    Update to git 54d8e790c8f52d0ebe27a0f32a678153b3c6f31f

--- unison.rb
+++ unison.rb
@@ -1,9 +1,9 @@
 class Unison < Formula
   desc "File synchronization tool for OSX"
-  version "2.51.3-git.26a29f7"
+  version "2.51.3-git.54d8e79"
   homepage "https://www.cis.upenn.edu/~bcpierce/unison/"
-  url "https://github.com/bcpierce00/unison/archive/26a29f79487484b7982c85e0dc879cf7aaaf584f.tar.gz"
-  sha256 "4b5dd5af7354cce25b261d0e7106df76b2d0386898f78e0ab952580907e9c757"
+  url "https://github.com/bcpierce00/unison/archive/54d8e790c8f52d0ebe27a0f32a678153b3c6f31f.tar.gz"
+  sha256 "b3f5e35c59fcb72f4c79bad999ed9c3ff4339968ca6b40e14434eab96e7a7c57"
   head "https://github.com/bcpierce00/unison.git", :branch => "master"
 
   depends_on "ocaml" => :build
