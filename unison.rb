8e0ee49e0f9f5a3082321040107f17be33d34a29
croadfeldt <chris@roadfeldt.com>
2020-11-15 01:03:38 -0600

    Bumped build number.

--- unison.rb
+++ unison.rb
@@ -1,6 +1,6 @@
 class Unison < Formula
   desc "File synchronization tool for OSX"
-  version "2.51.3-git.54d8e79"
+  version "2.51.5-git.54d8e79"
   homepage "https://www.cis.upenn.edu/~bcpierce/unison/"
   url "https://github.com/bcpierce00/unison/archive/54d8e790c8f52d0ebe27a0f32a678153b3c6f31f.tar.gz"
   sha256 "b3f5e35c59fcb72f4c79bad999ed9c3ff4339968ca6b40e14434eab96e7a7c57"
