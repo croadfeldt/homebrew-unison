e9aeb2b0da77ec467b3bbd7201323359c0725ff0
Chris Roadfeldt <chris@roadfeldt.com>
2021-10-12 10:47:32 -0500

    Updated to commit a39c5727c9d2f7c419f2cb28f33ebd0177486969

--- unison.rb
+++ unison.rb
@@ -1,9 +1,9 @@
 class Unison < Formula
   desc "File synchronization tool for OSX"
-  version "2.51.4-git.a881780"
+  version "2.51.4-git.a39c572"
   homepage "https://www.cis.upenn.edu/~bcpierce/unison/"
-  url "https://github.com/bcpierce00/unison/archive/a881780c8bfced48dcc0ed83130ef9e3487a36aa.tar.gz"
-  sha256 "cfe64752ff55d724338fc40195bd695ad70d0e4735b31d2d7fd971af20826000"
+  url "https://github.com/bcpierce00/unison/archive/a39c5727c9d2f7c419f2cb28f33ebd0177486969.tar.gz"
+  sha256 "1b354ecda2f6a78841f3aa358a9bd4f78934bd4c7295d002a67ad8f870b5a2c1"
   head "https://github.com/bcpierce00/unison.git", :branch => "master"
 
   depends_on "croadfeldt/ocaml/ocaml" => :build
